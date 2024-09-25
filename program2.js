d8f6d4fafb91b873595efe53e986f368e0ef58d7
AyushSoni <0112ec201003@gmail.com>
2024-09-25 13:03:35 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,34 @@
 const decodeTheRing = function (s, p) {
+  const match = (sIndex, pIndex) => {
+      // If we've reached the end of both strings, it's a match
+      if (sIndex === s.length && pIndex === p.length) return true;
 
-  
+      // If we've reached the end of the pattern but still have characters in the string
+      if (pIndex === p.length) return false;
 
+      // Handle '*' wildcard
+      if (p[pIndex] === '*') {
+          // Try matching '*' with zero characters (move pattern index)
+          if (match(sIndex, pIndex + 1)) return true;
+
+          // Try matching '*' with one or more characters (move string index)
+          while (sIndex < s.length) {
+              if (match(sIndex + 1, pIndex)) return true; // Match '*' with current character
+              sIndex++;
+          }
+          return false; // No match found
+      }
+
+      // Handle '?' wildcard or direct match
+      if (sIndex < s.length && (p[pIndex] === '?' || p[pIndex] === s[sIndex])) {
+          return match(sIndex + 1, pIndex + 1);
+      }
+
+      // If we reach here, it means there was no match
+      return false;
   };
-  
-  module.exports = decodeTheRing;
+
+  return match(0, 0);
+};
+
+module.exports = decodeTheRing;
