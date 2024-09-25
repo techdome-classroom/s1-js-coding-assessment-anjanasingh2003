33e830e6de85c3ebacddca21a819db461a9e3c4f
AyushSoni <0112ec201003@gmail.com>
2024-09-25 13:05:15 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -13,15 +13,14 @@ const decodeTheRing = function (s, p) {
               if (match(sIndex + 1, pIndex)) return true; 
               sIndex++;
           }
-          return false; // No match found
+          return false; 
       }
 
-      // Handle '?' wildcard or direct match
       if (sIndex < s.length && (p[pIndex] === '?' || p[pIndex] === s[sIndex])) {
           return match(sIndex + 1, pIndex + 1);
       }
 
-      // If we reach here, it means there was no match
+   
       return false;
   };
 
